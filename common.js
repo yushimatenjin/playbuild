1c375cc94a0995b83f489a047577f8f8dfe9ecc7
Mark Lundin <mark@kuva.io>
2022-05-06 14:15:43 +0100

    stati files now copied over

--- common.js
+++ common.js
@@ -1,5 +1,11 @@
+import copyStaticFiles from 'esbuild-copy-static-files'
+
 export default {
     entryPoints: ['./src/contentscripts', './src/serviceworkers', './src/ipcpm', './src/editor', './src/error-handler' ],
     outdir: 'build',
-    bundle: true
+    bundle: true,
+    plugins: [copyStaticFiles({
+        src: './static',
+        dest: './build'
+    })]
 }
