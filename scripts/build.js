8c47011ec27e4de65a404401cc3ecfc055874292
Mark Lundin <mark@kuva.io>
2022-05-06 16:41:52 +0100

    added zip for ext

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,17 @@
 import * as esbuild from "esbuild"
 import common, { plugins } from './common.js'
 import manifestPlugin from './manifest-plugin.js'
+import { zip } from 'zip-a-folder';
 
 esbuild.build({
     ...common,
     plugins: [...plugins, manifestPlugin(false)],
-    // minify: true,
-    // mangleProps: /_$/,
-}).then(result => {
+    minify: true,
+    mangleProps: /_$/,
+    // write: false,
+    // out: 'out'
+}).then(async result => {
+    await zip('./build', './pcpm.zip');
     console.log('Build Successful...')
 })
 
