30e334bdab5116ab81bed945a30b738bbbc0936a
Mark Lundin <mark@kuva.io>
2023-01-05 14:59:36 +0000

    stabiliy improvements

--- src/utils/package.js
+++ src/utils/package.js
@@ -1,41 +1,112 @@
-import { watchFile } from './fs'
 import { findAsset, isPkgJson } from '../utils.js'
+import path from 'path-browserify'
 
-export const watchJson = async (jsonFile, onChange) => {
-    const { value } = await watchFile(jsonFile, ({ value }) => onChange(JSON.parse(value)))
+const resolvePath = (asset, altPath) => {
+    const relativePath = (altPath ?? asset.get('path'))
+        .map(id => editor.call('assets:get', id).get('name')).join('/')
+    return path.resolve('/' + relativePath + '/' + asset.get('file.filename'))
+}
+
+const readFile = (asset) => {
+    return new Promise((resolve, reject ) => {
+
+        const resolveData = asset => editor.call('assets:contents:get', asset, (err, value) => {
+            const key = resolvePath(asset)
+            if(err) reject(err)
+            else resolve({ key, value })
+        })
+
+        const uid = asset.get('id')
+        const editorDoc = editor.call('documents:get', uid )
+        if (editorDoc?.data) {
+            const key = resolvePath(asset)
+            resolve({ key, value: editorDoc.data })
+        } else if (asset.get('file.filename')) {
+            resolveData(asset);
+        } else {
+            asset.once('file.filename:set', _ => resolveData(asset));
+        }
+    })
+}
+
+export const watchJson = async (jsonFile) => {
+    const { value } = await readFile(jsonFile)
     return JSON.parse(value)
 }
 
 export const watchPkgJson = async onChange => {
 
     if(!onChange || typeof onChange !== 'function') throw new Error(`'watchPkgJson' expects a function parameter`)
+    const connection = editor.call('realtime:connection')
+
+    const onAssetRenamed = name => {
+
+        const asset = findAsset(isPkgJson)
+
+        if(name === 'package.json') {
+            onAssetAdded(asset)
+        } else {
+            // onAssetRemoved(asset)
+        }
+    }
 
     const onAssetRemoved = asset => {
         if(!isPkgJson(asset)) return
         editor.unbind('assets:remove', onAssetRemoved)
+        asset.unbind('file.hash:set', onAssetContentChanged)
         editor.on('assets:add', onAssetAdded)
         onChange(null)
     }
 
-    const watch = async pkg => {    
-        editor.on('assets:remove', onAssetRemoved)
-        return watchJson(pkg, onChange)
+    const onAssetContentChanged = _ => {
+
+        const uid = asset.get('id')
+        const doc = connection.get('documents', uid)
+        // const key = resolvePath(asset)
+        if(doc?.data) onChange(doc.data)
     }
+
+    // const watch = async pkg => {    
+    //     editor.on('assets:remove', onAssetRemoved)
+    //     asset.on('file.hash:set', onAssetContentChanged)
+    //     return readFile(pkg, onChange)
+    // }
     
     const onAssetAdded = async asset => {
         if(isPkgJson(asset)){
             editor.unbind('assets:add', onAssetAdded)
-            const pkg = await watch(asset)
+            // const checkIfRenamedFromPackageJson = (a, b, c) => {
+            //     console.log('isPGF A', isPkgJson(asset), a, b, c)
+            //     if(!isPkgJson(asset)) {
+            //         asset.unbind('name:set', checkIfRenamedFromPackageJson)
+            //         onAssetRemoved(asset)
+            //     }
+            // }
+            asset.on('name:set', checkIfRenamedFromPackageJson)
+            asset.on('file.hash:set', onAssetContentChanged)
+            editor.on('assets:remove', onAssetRemoved)
+            const pkg = await readFile(asset)
             onChange(pkg)
+        } else {
+            // const checkIfRenamedToPackageJson = (a, b, c) => {
+            //     console.log('isPGF B', isPkgJson(asset), a, b, c)
+            //     if(isPkgJson(asset)) {
+            //         asset.unbind('name:set', checkIfRenamedToPackageJson)
+            //         onAssetAdded(asset)
+            //     }
+            // }
+            // asset.on('name:set', checkIfRenamedToPackageJson)
         }
     }
     
     const packageJson = findAsset(isPkgJson)
     if(packageJson) {
-        await watch(packageJson)
-        // onChange(result)
+        onAssetAdded(packageJson)
+    } else {
+        editor.on('assets:add', onAssetAdded)
+        const allJsonFiles = editor.call("assets:list").filter(asset => asset._data.type === 'json')
+        allJsonFiles.forEach(onAssetAdded)
     }
-    else editor.on('assets:add', onAssetAdded)
 }
 
 export const createPackageJson = (pkg = { dependencies:{}}) => {
@@ -79,6 +150,7 @@ export const getPkgJson = _ => {
 
         if (!pkg){
             resolve(null)
+            return null
             // await createPackageJson()
             // pkg = findAsset(isPkgJson)
         }
