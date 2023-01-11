24670c0bb8575cd22d9e37d4ef3a2e0b46c8aa0b
Mark Lundin <mark@kuva.io>
2023-01-11 13:35:55 +0000

    maintenance: code removal

--- src/codeeditor/components/package-manager.js
+++ src/codeeditor/components/package-manager.js
@@ -1,6 +1,4 @@
 // import NoPackageJson from './no-package';
-// import * as DiffMatchPatch from 'diff-match-patch-js-browser-and-nodejs/diff_match_patch.js';
-// import { diff2Op, findAsset, isPkgJson } from '../utils';
 import PackagePanel from './package-panel';
 import { Container, TextInput, InfoBox } from '@playcanvas/pcui'
 import '../styles/main.css'
