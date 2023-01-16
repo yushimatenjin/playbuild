427c06bd79455f5cc86ba4871dcdc7099623f298
Mark Lundin <mark@kuva.io>
2023-01-16 15:41:03 +0000

    small fix package linking out to npm

--- src/codeeditor/components/package-panel.js
+++ src/codeeditor/components/package-panel.js
@@ -3,7 +3,7 @@ export default class PackagePanel extends pcui.Container {
     constructor(module, version){
         super({ flex: true, flexDirection: 'row', alignItems: 'center', class: 'pcui-pcpm-package' })
 
-        const { name, versions, description, homepage } = module
+        const { name, versions } = module
         const options = Object.keys(versions).map(v => ({ t: v, v}))
         const title = new pcui.Label({ text: name, class: 'pcui-label' })
         const help = new pcui.Container({ class: 'help-icon'})
@@ -18,13 +18,13 @@ export default class PackagePanel extends pcui.Container {
         title.style.fontSize = '12px'
         combo.on('change', version => this.emit('change', { [name]: version }))
 
-        remove.on('click', _ => this.emit('click:remove'));
-        help.on('click', _ => window.open(`https://npmjs.com/pacakge/${name}`));
+        remove.on('click', _ => this.emit('click:remove'))
+        help.on('click', _ => window.open(`https://npmjs.com/package/${name}`))
 
-        this.append(title);
-        if(homepage) this.append(help);
-        this.append(combo);
-        this.append(remove);
+        this.append(title)
+        this.append(help)
+        this.append(combo)
+        this.append(remove)
 
     }
 }
